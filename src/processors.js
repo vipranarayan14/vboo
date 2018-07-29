26062d39a3c7e214237cf1005d688b689ad673ea
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-07-29 19:38:10 +0530

    add processor for 'about-word' tag.

--- src/processors.js
+++ src/processors.js
@@ -68,6 +68,34 @@ const processSadbaTable = ele => {
 
 };
 
+const processAboutWord = ele => {
+
+  const pratipadikam = ele.querySelector('pp').innerHTML;
+  const anta = ele.querySelector('antam').innerHTML;
+  const linga = ele.querySelector('linga').innerHTML;
+
+  ele.outerHTML =
+    `
+  <table>
+    <thead>
+      <tr>
+        <th align="center">प्रातिपदिकम्</th>
+        <th align="center">अन्तः</th>
+        <th align="center">लिङ्गः</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td align="center">${pratipadikam}</td>
+        <td align="center">${anta}</td>
+        <td align="center">${linga}</td>
+      </tr>
+    </tbody>
+  </table>
+  `;
+
+};
+
 const processors = [
 
   {
@@ -78,6 +106,11 @@ const processors = [
   {
     process: processSadbaTable,
     query: 'sabda-table'
+  },
+
+  {
+    process: processAboutWord,
+    query: 'about-word'
   }
 
 ];
