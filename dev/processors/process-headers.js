0abc5df6ac93d8d67865ab8ec5e2c18fbd143044
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-08-21 07:40:09 +0530

    disable lowercasing anchors.

--- dev/processors/process-headers.js
+++ dev/processors/process-headers.js
@@ -3,7 +3,6 @@ const processHeaders = ele => {
   const refinedText = ele.textContent
     .replace(/[^\x00-\x7F]/g, '') //Remove non-ASCII chars
     .trim() //Remove space around
-    .toLowerCase()
     .replace(/\s+/g, '-'); //Replace space with '-' (hyphens)
 
   const content = ele.innerHTML;
