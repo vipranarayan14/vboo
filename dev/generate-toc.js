95e99108ab70a57f4a952ac49c1fd5e1f0632516
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-08-22 15:31:30 +0530

    try adding support for auto-generating toc.

---
+++ dev/generate-toc.js
@@ -0,0 +1,32 @@
+// // const traverse = require('traverse');
+// // const dirTree = require('directory-tree');
+
+// // const tree = dirTree(
+
+// //   './src/docs/',
+
+// //   {
+// //     extensions: /\.html$/
+// //   },
+
+// //   (item, PATH) => {
+
+// //     console.log(item);
+
+// //   });
+
+// // // traverse().map(function (x) {
+
+// // //   if (typeof x === 'string') {
+
+// // //     console.log(x, this.key);
+
+// // //   }
+
+// // // });
+
+// const { readdir } = require('fs');
+
+// readdir('./src/docs/home.html', (err, items) => {
+
+// });
